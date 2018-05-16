caa9e8564643ea96c06079f134f8a017670580de
Restricted <Restricted@clientMachine1.local>
2018-05-16 08:43:12 -0400

    v1.0 initial release

--- E46M3/ViewController.swift
+++ E46M3/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    
+    @IBAction func navMPCs(_ sender: UIButton) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // === Hide navigation bar on rootViewController ===
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated);
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    // ==================================================
+    
+    
 
 }
 
